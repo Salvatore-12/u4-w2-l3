9682b43ab5572b6d9036fb4e57f8d9c541b12491
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2023-12-06 16:54:13 +0100

    3 commit

--- src/Es1/Order.java
+++ src/Es1/Order.java
@@ -3,16 +3,76 @@ package Es1;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
 
 public class Order {
-    public static void main(String[] args){
-        String status;
-        Long id;
-        LocalDate orderDate;
-        LocalDate deliveryDate;
-        Customer customer;
 
-        List<Product> prodotto= new ArrayList<>();
 
+    private String status;
+    private Long id;
+    private  LocalDate orderDate;
+    private  LocalDate deliveryDate;
+
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(List<Product> product, Customer customer) {
+        this.products = products;
+        this.customer = customer;
+        Random cod = new Random();
+        this.id = cod.nextLong(1, 120);
+        this.orderDate = LocalDate.now();
+        this.deliveryDate = orderDate.plusDays(5);
+        this.status = "is loading";
     }
-}
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(LocalDate deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+    }
+
--- src/Es1/Product.java
+++ src/Es1/Product.java
@@ -1,10 +1,53 @@
 package Es1;
 
-public class Product {
-    public static void main(String[] args){
-        String Name;
-        String Category;
-        Double price;
-        Long id;
-    }
+import java.util.List;
+import java.util.Random;
+
+public class Product extends Order {
+       private String name;
+       private String category;
+       private Double price;
+       private Long id;
+
+       public Product(List<Product> product, Customer customer, String name, String category, Double price, Long id) {
+              super(product, customer);
+              this.name = name;
+              this.category = category;
+              this.price = price;
+              Random cod = new Random();
+              this.id = id;
+       }
+
+       public String getName() {
+              return name;
+       }
+
+       public void setName(String name) {
+              name = name;
+       }
+
+       public String getCategory() {
+              return category;
+       }
+
+       public void setCategory(String category) {
+              category = category;
+       }
+
+       public Double getPrice() {
+              return price;
+       }
+
+       public void setPrice(Double price) {
+              this.price = price;
+       }
+
+       public Long getId() {
+              return id;
+       }
+
+       public void setId(Long id) {
+              this.id = id;
+       }
+
 }
